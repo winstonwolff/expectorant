aa4c57c05d3ba62df22cff1021f41da0d86a2d09
Winston <winstonw@stratolab.com>
2016-08-17 17:26:42 -0700

    Plan to-dos

--- sample_test.py
+++ sample_test.py
@@ -2,11 +2,9 @@
 
 class SodaFountain:
     def __init__(self):
-#         print("!!! SodaFountain: created.")
         self.syrup = 10
 
     def pour(self, quantity):
-#         print("!!! SodaFounding: pouring", quantity)
         self.syrup -= quantity
 
 # Our tests
@@ -27,31 +25,17 @@ def _():
 
         @before
         def _():
-#             print("!!! when-adding-before called")
             soda_fountain.pour(1)
 
         def subject(val):
-#             print("!!! subject called with", val)
             soda_fountain.pour(val)
 
         @it("pouring another leaves 8 left")
         def _(expect):
             subject(1)
             expect.is_equal(soda_fountain.syrup, 8)
-#             print("!!! it 1 called. sum=", soda_fountain.syrup)
 
         @it("pouring 5 leaves 4 left")
         def _(expect):
             subject(5)
             expect.is_equal(soda_fountain.syrup, 4)
-#             print("!!! it 5 called. sum=", soda_fountain.syrup)
-#             expect(adder.sum, equals(0))
-
-#             @it("changes score")
-#             def _():
-#                 expect(subject, changes(lambda: bowling.score, from=0, to=1))
-
-#             @it("is not over")
-#             def _():
-#                 expect(bowling.is_game_over(), truthy)
-
--- todo.md
+++ todo.md
@@ -1,19 +1,26 @@
 To Do
 -----
-*** exception reporting at end needs better visuals
-    - context should be indented, and colorized
 
-- Make samples with exceptions in various places, e.g.
-    - inside test that is not expecting it
-    - inside setup
-    - inside teardown
-    - inside testing framework
+- better expect() syntax
+
+    - check for side-effects to change
+
+    - check for raising exceptions
+
+- allow users to write new test functions easily
+
+- decouple testing from reporting
+
+- use codec hack to hide `def _():`
+
+- better way to share values through test chain, e.g. pass a mutable 'self'
+
+- Handle exceptions
+    - Make samples with exceptions in various places, e.g.
+        - inside test that is not expecting it
+        - inside setup
+        - inside teardown
+        - inside testing framework
 
 - Testing system with state / side-effects
 
-- More matchers. Maybe pyhamcrest
-    with context('value', a, 'equals', b):
-        expect(a, equals, b, msg, more_msg)
-        expect(a, has_jquery_descriptor('p.is_disabled'), msg, more_msg)
-        expect(a).to(equal, b).msg(msg, more_msg)
-        expect('values are equal', a).to(equal, b).msg(msg, more_msg)
