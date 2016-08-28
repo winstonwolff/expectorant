eb321d4742b5cca1f39d46cd9fae0f66d3d15761
Winston <winstonw@stratolab.com>
2016-08-27 21:31:26 -0700

    Preparing for PyPi

--- setup.py
+++ setup.py
@@ -15,6 +15,15 @@ setup(
 
     keywords = ['testing', 'rspec', 'tdd'],
     entry_points = { 'console_scripts': [ 'expectorant=expectorant.runner:main' ] },
-    classifiers = [],
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: POSIX',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Topic :: Software Development :: Testing',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Utilities',
+                   'Programming Language :: Python :: 3.5',
+                  ]
 )
 
